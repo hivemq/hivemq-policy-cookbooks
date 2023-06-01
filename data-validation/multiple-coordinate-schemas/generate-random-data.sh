a50cc999325112eece718d4f14381f80bc0a5c05
Stefan Frehse <sfrehse@users.noreply.github.com>
2023-06-01 12:50:39 +0200

    add quality metric and PromQL example (#12)

---
+++ data-validation/multiple-coordinate-schemas/generate-random-data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+while true; do
+  mqtt pub -h localhost -t location -m "{\"latitude\": $(( $RANDOM % 50 + 20 )), \"longitude\": -120}" -q 1;
+
+  echo "wait";
+
+  sleep 0.5;
+done
