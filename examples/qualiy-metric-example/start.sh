9d11a17e42e7dfcaf36016e7e3c704d4c109188a
gitseti <till.seeberger@hivemq.com>
2023-06-06 20:57:15 +0200

    data quality example > move health-check to docker compose

--- examples/qualiy-metric-example/start.sh
+++ examples/qualiy-metric-example/start.sh
@@ -1,10 +1,5 @@
 docker compose -f docker/docker-compose.yml up -d
 
-echo "Waiting for HiveMQ REST API to become available..."
-until $(curl --output /dev/null --silent --head --fail http://localhost:8889/api/v1/health/readiness); do
-    sleep 1
-done
-
 SCHEMA_ID=$(curl -X POST --silent --data @./config/schema.json -H "Content-Type: application/json" http://localhost:8888/api/v1/data-validation/schemas | jq ".id")
 echo "Created schema $SCHEMA_ID."
 
