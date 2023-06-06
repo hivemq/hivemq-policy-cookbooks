85758bbc616cbbc09a9eabe7506e809ab57cf786
gitseti <till.seeberger@hivemq.com>
2023-06-06 16:42:23 +0200

    data quality example > add aimple mqtt message generation script

---
+++ examples/qualiy-metric-example/mqtt-generator/mqtt-generator.py
@@ -0,0 +1,76 @@
+import random
+import string
+import time
+import json
+import paho.mqtt.client as mqtt
+
+# MQTT broker settings
+broker_address = "127.0.0.1"
+broker_port = 1883
+topic = "mqtt/demo"
+
+cutting_machine_ids = set()
+cleaning_machine_ids = set()
+for i in range(100):
+    cutting_machine_ids.add("clean-" + ''.join(random.choice(string.ascii_letters) for _ in range(10)))
+    cleaning_machine_ids.add("cut-" + ''.join(random.choice(string.ascii_letters) for _ in range(10)))
+
+def random_cutting_machine_message():
+    message = {
+        "id": random.choice(list(cutting_machine_ids)),
+        "type": "cutting-machine",
+        "version": random.choice(["1", "2", "3", "4", 4.5, "5"]),
+        "timestamp": time.time(),
+        "value": {
+            "pressure": str(random.randint(0, 4000)) + "psi",
+            "packml_state": "EXECUTE"
+        }
+    }
+    return json.dumps(message)
+
+def random_cleaning_machine_message():
+    message = {
+        "id": random.choice(list(cleaning_machine_ids)),
+        "type": "cleaning-machine",
+        "version": random.choice(["1", "2", "3", 4]),
+        "timestamp": time.time(),
+        "value": {
+            "consumption": "1501m",
+            "packml_state": "EXECUTE"
+        }
+    }
+    return json.dumps(message)
+
+def generate_message():
+    generator = random.choice([random_cutting_machine_message, random_cleaning_machine_message])
+    return generator()
+
+# Publish MQTT messages
+def publish_mqtt_message(client):
+    while True:
+        message = generate_message()
+        client.publish(topic, message)
+        print(f"Published: {message}")
+        time.sleep(0.5)
+
+# Connect callback
+def on_connect(client, userdata, flags, reasonCode):
+    if reasonCode == 0:
+        print("Connected to MQTT broker")
+        client.subscribe(topic)
+    else:
+        print("Failed to connect to MQTT broker")
+
+# MQTT client setup
+client = mqtt.Client()
+
+# Set up callbacks
+client.on_connect = on_connect
+
+# Connect to MQTT broker
+client.connect(broker_address, broker_port)
+
+client.loop_start()
+
+client.subscribe("#")
+publish_mqtt_message(client)
